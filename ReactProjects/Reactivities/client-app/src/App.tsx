d27a5c4d4b37f3d012c8c28ca7cf7d8778642377
Daiana <dayana01001@gmail.com>
2024-01-04 12:03:21 +0100

    Added program

---
+++ ReactProjects/Reactivities/client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import 'semantic-ui-css/semantic.min.css';
+import {Header, List} from "semantic-ui-react";
+
+function App() {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5001/api/activities')
+            .then(response => {
+                console.log(response);
+                setActivities(response.data)
+            })
+    }, [])
+
+    return (
+        <div>
+            <Header as='h1' icon='users' content='Reactivities' />
+            <List>
+                {activities.map((activity: any) => (
+                    <List.Item key={activity.id}>
+                        {activity.title}
+                    </List.Item>
+                ))}
+            </List>
+        </div>
+    )
+}
+
+export default App
